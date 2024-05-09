8185ee56f593a36e6ed38981189edfacfe6ddc16
Cotne Basiashvili <basiashvilic@gmail.com>
2024-05-09 13:27:34 +0400

    change git ignore file

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,10 @@
 
 const nextConfig = {
   images: {
-    domains: ["s3-alpha-sig.figma.com"], // Allow images from this domain
+    domains: ["s3-alpha-sig.figma.com"],
   },
   compiler: {
-    // Use the styled-components babel plugin
-    // This enables server-side rendering for styled-components
     styledComponents: {
-      // Set the option for ssr
       ssr: true,
     },
   },
